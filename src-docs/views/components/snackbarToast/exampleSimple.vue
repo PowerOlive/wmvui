4e6770ac90fc51e48ab8c03fde4391d21d4d8016
wm <531431988@qq.com>
2017-06-09 17:21:32 +0800

    添加toast

--- src-docs/views/components/snackbarToast/exampleSimple.vue
+++ src-docs/views/components/snackbarToast/exampleSimple.vue
@@ -1,9 +1,12 @@
 <template>
 <div>
-  <mu-raised-button label="show snackbar" class="demo-snackbar-button" @click="showSnackbar"/>
-  <mu-raised-button label="show toast" class="demo-snackbar-button" @click="showToast"/>
-  <mu-snackbar v-if="snackbar" message="一段简单的文本" action="关闭" @actionClick="hideSnackbar" @close="hideSnackbar"/>
-  <mu-toast v-if="toast" message="一段简单的文本" @close="hideToast"/>
+  <mu-raised-button label="默认" @click="showToast()"/>
+  <mu-raised-button label="顶部" @click="showToast('top')"/>
+  <mu-raised-button label="底部" @click="showToast('bottom')"/>
+
+
+
+  <mu-toast v-model="toast.show" :position="toast.position" :time="toast.time" :message="toast.message" />
 </div>
 </template>
 
@@ -11,28 +14,21 @@
 export default {
   data () {
     return {
-      snackbar: false,
-      toast: false
+      toast: {
+        show: false,
+        time: 2000,
+        message: '默认中间显示'
+      }
     }
   },
   methods: {
-    showSnackbar () {
-      this.snackbar = true
-      if (this.snackTimer) clearTimeout(this.snackTimer)
-      this.snackTimer = setTimeout(() => { this.snackbar = false }, 2000)
-    },
-    hideSnackbar () {
-      this.snackbar = false
-      if (this.snackTimer) clearTimeout(this.snackTimer)
-    },
-    showToast () {
-      this.toast = true
-      if (this.toastTimer) clearTimeout(this.toastTimer)
-      this.toastTimer = setTimeout(() => { this.toast = false }, 2000)
-    },
-    hideToast () {
-      this.toast = false
-      if (this.toastTimer) clearTimeout(this.toastTimer)
+    showToast (val) {
+      this.toast.show = false
+      this.toast = Object.assign(this.toast, {
+        show: true,
+        position: val,
+        message: '11111'
+      })
     }
   }
 }
--- src/toast/toast.vue
+++ src/toast/toast.vue
@@ -1,6 +1,6 @@
 <template>
 <transition name="mu-toast">
-  <div class="mu-toast" v-clickoutside="clickOutSide" :style="{'z-index': zIndex}">
+  <div class="mu-toast"  v-show="show" :class="toastClass" :style="{'z-index': zIndex}">
     {{message}}
   </div>
 </transition>
@@ -8,26 +8,73 @@
 
 <script>
 import {getZIndex} from '../internal/popup/utils'
-import clickoutside from '../internal/clickoutside'
 export default {
   name: 'mu-toast',
   props: {
+    value: Boolean,
     message: {
       type: String
-    }
-  },
-  methods: {
-    clickOutSide () {
-      this.$emit('close', 'clickOutSide')
+    },
+    position: String,
+    time: {
+      type: Number,
+      default: 2000
+    },
+    type: {
+      type: String,
+      default: 'success'
+    },
+    isShowMask: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
     return {
+      show: false,
       zIndex: getZIndex()
     }
   },
-  directives: {
-    clickoutside
+  created () {
+    if (this.value) {
+      this.show = true
+    }
+  },
+  computed: {
+    toastClass () {
+      return {
+        'vui-toast-warn': this.type === 'warn',
+        'vui-toast-cancel': this.type === 'cancel',
+        'vui-toast-success': this.type === 'success',
+        'vui-toast-text': this.type === 'text',
+        'vux-toast-top': this.position === 'top',
+        'vux-toast-bottom': this.position === 'bottom'
+      }
+    },
+    style () {
+      if (this.type === 'text' && this.width === 'auto') {
+        return { padding: '10px' }
+      }
+    }
+  },
+  watch: {
+    show (val) {
+      if (val) {
+        this.$emit('input', true)
+        this.$emit('on-show')
+        // this.fixSafariOverflowScrolling('auto')
+        clearTimeout(this.timeout)
+        this.timeout = setTimeout(() => {
+          this.show = false
+          this.$emit('input', false)
+          this.$emit('on-hide')
+          // this.fixSafariOverflowScrolling('touch')
+        }, this.time)
+      }
+    },
+    value (val) {
+      this.show = val
+    }
   }
 }
 </script>
@@ -42,32 +89,7 @@ export default {
   color: @alternateTextColor;
   border-radius: 24px;
   .ellipsis();
-  max-width: 568px;
-  width: 100%;
   position: fixed;
-  left: 0;
-  bottom: 0;
-}
-
-
-@media only screen and (max-width: 992px) and (min-width: 601px) {
-  .mu-toast {
-    width: auto;
-    min-width: 288px;
-    left: 5%;
-    bottom: 7%;
-  }
-}
-@media only screen and (min-width: 993px) {
-  .mu-toast {
-    width: auto;
-    min-width: 8%;
-    top: 10%;
-    right: 7%;
-    left: auto;
-    bottom: auto;
-    min-width: 288px;
-  }
 }
 
 .mu-toast-enter-active,.mu-toast-leave-active{
@@ -80,4 +102,20 @@ export default {
   transform: translate3d(0, 100%, 0);
   opacity: 0;
 }
+
+</style>
+<style lang="scss">
+.mu-toast{
+  top:50%;
+  left:50%;
+  transform: translate3D(-50%,-50%,0);
+  &.vux-toast-top {
+    top:5%;
+  }
+  &.vux-toast-bottom {
+    top: auto;
+    bottom:5%;
+  }
+}
+
 </style>
